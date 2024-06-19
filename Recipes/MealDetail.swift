2516157babafe41b2a95d0f532f8f506decf23a9
ida5584 <idama@berkeley.edu>
2024-06-19 16:46:21 -0700

    Ingredients shows up but there are unwanted colons for extra ingredients

--- Recipes/MealDetail.swift
+++ Recipes/MealDetail.swift
@@ -14,40 +14,69 @@ struct MealDetail: Codable {
     let strInstructions: String
     let ingredients: [String]
     let measures: [String]
-    
+
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         idMeal = try container.decode(String.self, forKey: .idMeal)
         strMeal = try container.decode(String.self, forKey: .strMeal)
         strMealThumb = try container.decode(String.self, forKey: .strMealThumb)
         strInstructions = try container.decode(String.self, forKey: .strInstructions)
-        
+
         var ingredients = [String]()
         var measures = [String]()
-        
+
         for index in 1...20 {
-            if let ingredientKey = CodingKeys(stringValue: "strIngredient\(index)"),
-               let ingredient = try container.decodeIfPresent(String.self, forKey: ingredientKey),
-               !ingredient.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-                ingredients.append(ingredient)
+            let ingredientKeyString = "strIngredient\(index)"
+            let measureKeyString = "strMeasure\(index)"
+            
+//            -- Debugging --
+            if let ingredientKey = CodingKeys(stringValue: ingredientKeyString) {
+                if let ingredient = try container.decodeIfPresent(String.self, forKey: ingredientKey) {
+                    print("Ingredient Key: \(ingredientKeyString) - Value: \(ingredient)")
+                } else {
+                    print("Ingredient Key: \(ingredientKeyString) not found or value is nil")
+                }
+            } else {
+                print("Failed to create ingredient key for: \(ingredientKeyString)")
             }
-            if let measureKey = CodingKeys(stringValue: "strMeasure\(index)"),
-               let measure = try container.decodeIfPresent(String.self, forKey: measureKey),
-               !measure.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-                measures.append(measure)
+//           -- Debugging --
+            
+            if let ingredientKey = CodingKeys(stringValue: ingredientKeyString) {
+                if let ingredient = try container.decodeIfPresent(String.self, forKey: ingredientKey) {
+                    ingredients.append(ingredient)
+                }
+            }
+            if let measureKey = CodingKeys(stringValue: measureKeyString) {
+                if let measure = try container.decodeIfPresent(String.self, forKey: measureKey) {
+                    measures.append(measure)
+                }
             }
         }
-        
+    
         self.ingredients = ingredients
         self.measures = measures
     }
-    
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(idMeal, forKey: .idMeal)
+        try container.encode(strMeal, forKey: .strMeal)
+        try container.encode(strMealThumb, forKey: .strMealThumb)
+        try container.encode(strInstructions, forKey: .strInstructions)
+
+        // implement encoding for ingredients and measures if needed
+        // Example: If you want to encode ingredients as an array of strings
+        // try container.encode(ingredients, forKey: .ingredients)
+    }
+
     enum CodingKeys: String, CodingKey {
         case idMeal, strMeal, strMealThumb, strInstructions
+        // Add more cases for dynamic keys if needed
+        case strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, strIngredient19, strIngredient20
+        case strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strMeasure16, strMeasure17, strMeasure18, strMeasure19, strMeasure20
     }
 }
 
 struct MealDetailResponse: Codable {
     let meals: [MealDetail]
 }
-
